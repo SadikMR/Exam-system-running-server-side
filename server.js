50f6105ce5987810def0eb9cacd9e0ca672cc510
Md. Sadik Mahmud Raihan <sadikmahmudraihan@gmail.com>
2025-11-22 14:51:00 +0600

    deploy2

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ app.use(bodyParser.urlencoded({ limit: "20mb", extended: true })); // Parse URL-
 
 // MongoDB connection using Mongoose
 const connectDB = async () => {
+  console.log("Render MongoDB URI:", process.env.MONGODB_URI);
   try {
-    console.log("Render MongoDB URI:", process.env.MONGODB_URI);
     await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
